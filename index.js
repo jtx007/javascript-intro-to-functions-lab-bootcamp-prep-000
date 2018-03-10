1b2725951d6e9e913965d4aa35b34e6bfe052b55
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-10 08:12:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUppercase())
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log(string.toLowercase())
+  console.log(string.toLowerCase())
 }
